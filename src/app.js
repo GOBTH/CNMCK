f294035a0134ec816a0e78924599e365c7c4d01a
GOBTH <dangduykhang97@gmail.com>
2018-12-19 16:24:35 +0700

    login-signup

--- src/app.js
+++ src/app.js
@@ -4,13 +4,17 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './component/Home';
 import Infor from './component/Infor';
 import Navbar from './component/CustomNavbar';
+import Login from './component/Login/Login';
+import Signup from './component/Sign up/Signup';
 class App extends Component {
     render() {
         return (
         <Router>
             <div>
-                <Navbar/>
-                <Route exact path="/" component={Home} />
+                <Navbar/>     
+                <Route exact path="/login" component={Login} />
+                <Route path="/signup" component={Signup} />
+                <Route path="/" component={Home} />
                 <Route path="/infor" component={Infor} />
             </div>
         </Router>
